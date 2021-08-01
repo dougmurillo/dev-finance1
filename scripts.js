cad117e31e677cdc10f9e89aaabddd09ae5276b5
Douglas <douglas.murillo94@gmail.com>
2021-08-01 15:54:55 -0300

    primeiras alterações

--- scripts.js
+++ scripts.js
@@ -26,6 +26,12 @@ const transactions = [
         amount: 20000,
         date: '23/01/2021'
     },
+    {
+        id: 4,
+        description: 'App',
+        amount: 2000000,
+        date: '23/01/2021'
+    },
 ]
 
 
@@ -44,18 +50,48 @@ const Transaction = {
 // Eu preciso pegar as minhas transações do meu objeto aqui no javascript e colocar la no HTML.
 
 const DOM = {
-    innerHTMLTransaction () {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction (transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">23/01/2021</td>
-            <td>
-            <img src="img/minus.svg" alt="Remover transação">
-            </td>
-        </tr>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+                <img src="img/minus.svg" alt="Remover transação">
+        </td>
   
      `
+        return html
     }
 }
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+            value = Number(value) / 100
+
+            value = value.toLocaleString("pt-BR", {
+                style:"currency",
+                currency: "BRL"
+            })
+
+            return signal + value
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
