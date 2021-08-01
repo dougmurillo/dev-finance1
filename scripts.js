f128559cfb0ecc7e3938ab4fbbb6042769227757
Douglas <douglas.murillo94@gmail.com>
2021-08-01 18:44:24 -0300

    segunda alteração

--- scripts.js
+++ scripts.js
@@ -9,25 +9,21 @@ const Modal = {
 
 const transactions = [
     {
-        id: 1,
         description: 'Luz',
         amount: -50000,
         date: '23/01/2021'
     },
     {
-        id: 2,
         description: 'Website',
         amount: 50000,
         date: '23/01/2021'
     },
     {
-        id: 3,
         description: 'Internet',
-        amount: 20000,
+        amount: -20000,
         date: '23/01/2021'
     },
     {
-        id: 4,
         description: 'App',
         amount: 2000000,
         date: '23/01/2021'
@@ -35,15 +31,50 @@ const transactions = [
 ]
 
 
-const Transaction = {
+const Transaction = { 
+    all: transactions,
+
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
     incomes() {
-        //somar as entradas
+        let income = 0;
+        // pegar todas as Transações
+        //para cada transação
+        Transaction.all.forEach(transaction => {
+            //se ela for maior que zero
+            if( transaction.amount > 0 ) {
+                //somar a uma variável e retornar a variável 
+                income += transaction.amount;
+            }
+        })
+        return income;
     },
     expenses() {
-        //somar as saídas
+        let expense = 0;
+        // pegar todas as Transações
+        //para cada transação
+        Transaction.all.forEach(transaction => {
+            //se ela for maior que zero
+            if( transaction.amount < 0 ) {
+                //somar a uma variável e retornar a variável 
+                expense += transaction.amount;
+            }
+        })
+        return expense;
+       
     },
     total() {
-        //entradas - saídas
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
@@ -73,6 +104,22 @@ const DOM = {
   
      `
         return html
+    },
+
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+             .getElementById('totalDisplay')
+             .innerHTML =  Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
@@ -92,6 +139,22 @@ const Utils = {
     }
 }
 
-transactions.forEach(function(transaction){
-    DOM.addTransaction(transaction)
-})
+const App = {
+    init() {
+
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+Transaction.remove(0)
